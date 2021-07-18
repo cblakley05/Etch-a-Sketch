987fc0ee9cbb0df9d983814fd8343603ef24b7ba
Caleb <=>
2021-07-18 19:27:42 -0400

    Added grid size select and color options

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
 
 const container = document.querySelector('.container');
-const clear = document.querySelector('.clear')
+const gridSelect = document.querySelector('.grid-size');
+const blackBTN = document.querySelector('.color-black');
+const rgbBTN = document.querySelector('.color-rgb');
+const invertedBTN = document.querySelector('.color-inverted');
+const clear = document.querySelector('.clear');
+
+// grid size select
+gridSelect.addEventListener('change', () => {
+    renderGrid(gridSelect.value);
+});
+
+// grid color object
+const gridColors = {
+    background: '#FFF',
+    color: '#222',
+    isRGB: false,
+    randColor() {
+        function randValue() {
+            return Math.floor(Math.random() * 256)
+        }
+        return `rgb(${randValue()}, ${randValue()}, ${randValue()})`
+    }
+};
+
+// selecting square color
+blackBTN.addEventListener('click', () => {
+    gridColors.startColor = '#FFF';
+    gridColors.color = '#222';
+    gridColors.isRGB = false;
+    renderGrid(gridSelect.value);
+});
+
+rgbBTN.addEventListener('click', () => {
+    gridColors.startColor = '#FFF';
+    gridColors.isRGB = true;
+    renderGrid(gridSelect.value)
+})
+
+invertedBTN.addEventListener('click', () => {
+    gridColors.startColor = '#222';
+    gridColors.color = '#FFF';
+    gridColors.isRGB = false;
+    renderGrid(gridSelect.value);
+})
 
 // reset button
 clear.addEventListener('click', () => {
-    let squares = document.querySelectorAll('.square');
-    for (let i = 0; i < squares.length; i++) {
-        squares[i].classList.remove("hovered");
-    }
+    renderGrid(gridSelect.value);
 })
 
 // generating squares
@@ -17,6 +57,7 @@ function createSquare(sideLength) {
     newSquare.style.width = size;
     newSquare.style.height = size;
     newSquare.style.margin = "0";
+    newSquare.style.backgroundColor = gridColors.startColor;
     newSquare.classList.add('square');
     return newSquare
 }
@@ -25,7 +66,6 @@ function createSquare(sideLength) {
 function renderGrid(sideLength) {
     // clear old squares from container
     while (container.firstChild) {
-        console.log("looping")
         container.removeChild(container.lastChild)
     };
 
@@ -41,8 +81,13 @@ function renderGrid(sideLength) {
 
     for (let i = 0; i < squares.length; i++) {
         squares[i].addEventListener('mouseover', function () {
-            squares[i].classList.add("hovered");
-        })
+            if (gridColors.isRGB) {
+                squares[i].style.backgroundColor = gridColors.randColor();
+                console.log(squares[i].style.backgroundColor)
+            } else {
+                squares[i].style.backgroundColor = gridColors.color;
+            }
+        });
     }
 }
 
